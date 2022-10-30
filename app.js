fd91a73b2b12c8a92dec66f0edd7865def05e83b
Holutunde <Holutunde99@gmail.com>
2022-10-30 02:43:44 +0100

    nodemon

--- app.js
+++ app.js
@@ -3,10 +3,20 @@ const datas = require('./server/routes/datas')
 
 const app = express()
 
-const PORT = 5000
+const port = 5000
 
 app.use(express.json())
 
 app.use('/', datas)
 
-app.listen(PORT, () => console.log(`server is listening on port ${PORT}`))
+const start = async () => {
+  try {
+    app.listen(port, () =>
+      console.log(`Server is listening on port ${port}...`),
+    )
+  } catch (error) {
+    console.log(error)
+  }
+}
+
+start()
